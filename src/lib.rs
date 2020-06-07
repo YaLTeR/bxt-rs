0baddeee58a6f921a8d7fe175f89c8bfef997d71
Ivan Molodetskikh <yalterz@gmail.com>
2020-06-07 15:16:19 +0300

    Top-level mods don't need to be pub

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@
 #[macro_use]
 extern crate log;
 
-pub mod ffi;
-pub mod hooks;
-pub mod modules;
-pub mod utils;
+mod ffi;
+mod hooks;
+mod modules;
+mod utils;
 
 pub use hooks::engine::{
     Host_Shutdown, LoadEntityDLLs, Memory_Init, ReleaseEntityDlls, SV_Frame, V_FadeAlpha,
