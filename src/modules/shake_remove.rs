23fac432dcbb1e5fe2f6267cc87d802782d9535f
Marat Mustafin <nopenope8@gmail.com>
2021-02-21 16:03:44 +0300

    add bxt_shake_remove

---
+++ src/modules/shake_remove.rs
@@ -0,0 +1,35 @@
+//! `bxt_shake_remove`
+
+use super::Module;
+use crate::{
+    hooks::engine,
+    modules::cvars::{self, CVar},
+    utils::*,
+};
+
+pub struct ShakeRemove;
+impl Module for ShakeRemove {
+    fn name(&self) -> &'static str {
+        "bxt_shake_remove"
+    }
+
+    fn cvars(&self) -> &'static [&'static CVar] {
+        static CVARS: &[&CVar] = &[&BXT_SHAKE_REMOVE];
+        &CVARS
+    }
+
+    fn is_enabled(&self, marker: MainThreadMarker) -> bool {
+        engine::V_ApplyShake.is_set(marker) && cvars::CVars.is_enabled(marker)
+    }
+}
+
+static BXT_SHAKE_REMOVE: CVar = CVar::new(b"bxt_shake_remove\0", b"0\0");
+
+/// Returns `true` if shake should currently be removed.
+pub fn is_active(marker: MainThreadMarker) -> bool {
+    if !ShakeRemove.is_enabled(marker) {
+        return false;
+    }
+
+    BXT_SHAKE_REMOVE.as_bool(marker)
+}
