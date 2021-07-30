108f9792e0697b64746c04be85cf9fa0a08f07e8
Ivan Molodetskikh <yalterz@gmail.com>
2021-07-30 22:09:27 +0300

    Fix formatting

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,11 +1,11 @@
 //! Raw bindings to C structs and functions.
-//! 
+//!
 //! These are generated with a command similar to:
-//! 
+//!
 //! ```sh
 //! bindgen /path/to/halflife/pm_shared/pm_defs.h --whitelist-type playermove_s -- --target=i686-unknown-linux-gnu -I/path/to/halflife/{public,common} -include mathlib.h -include const.h > src/ffi/playermove.rs
 //! ```
-//! 
+//!
 //! and then manually cleaned up a bit.
 
 pub mod com_model;
