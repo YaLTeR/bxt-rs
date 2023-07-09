c33a04742e8d9a8cdbcc905244b14f6feec46c0e
Ivan Molodetskikh <yalterz@gmail.com>
2023-07-09 15:03:07 +0400

    tracer: Initialize lazily

--- src/modules/player_movement_tracing/tracer.rs
+++ src/modules/player_movement_tracing/tracer.rs
@@ -1,3 +1,5 @@
+use std::cell::Cell;
+
 use bxt_strafe::{Hull, Trace, TraceResult};
 use glam::Vec3;
 
@@ -5,9 +7,10 @@ use super::PlayerMovementTracing;
 use crate::modules::Module;
 use crate::utils::marker::MainThreadMarker;
 
-#[derive(Clone, Copy)]
 pub struct Tracer {
     marker: MainThreadMarker,
+    remove_distance_limit: bool,
+    ensured: Cell<bool>,
 }
 
 impl Tracer {
@@ -21,14 +24,22 @@ impl Tracer {
             return None;
         }
 
-        super::maybe_ensure_server_tracing(marker, remove_distance_limit);
-
-        Some(Self { marker })
+        Some(Self {
+            marker,
+            remove_distance_limit,
+            ensured: Cell::new(false),
+        })
     }
 }
 
 impl Trace for Tracer {
     fn trace(&self, start: Vec3, end: Vec3, hull: Hull) -> TraceResult {
+        if !self.ensured.replace(true) {
+            // Ensure server tracing lazily to support drawing the TAS editor outside gameplay to
+            // some extent (and also to avoid unnecessary calls).
+            unsafe { super::maybe_ensure_server_tracing(self.marker, self.remove_distance_limit) };
+        }
+
         unsafe { super::player_trace(self.marker, start, end, hull) }
     }
 }
